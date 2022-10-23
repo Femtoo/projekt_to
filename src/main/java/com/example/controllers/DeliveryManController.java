43b4b9496c2577b814adb3adc3d92e8e1c5cf67e
MichalZdanuk <zdanuk_m@wp.pl>
2022-10-23 23:21:25 +0200

    added validating negative delivery_man_nr

--- src/main/java/com/example/controllers/DeliveryManController.java
+++ src/main/java/com/example/controllers/DeliveryManController.java
@@ -7,14 +7,17 @@ import java.sql.*;
 import java.util.ArrayList;
 import java.util.List;
 
-public class DeliveryManController implements IDeliveryManController{
+public class DeliveryManController implements IDeliveryManController {
 
     public boolean create(DeliveryMan deliveryMan) {
         String sql = "INSERT INTO delivery_man(name, last_name, delivery_man_nr) " +
-            "VALUES(?,?,?)";
+                "VALUES(?,?,?)";
+        if (deliveryMan.getDelivery_man_nr() <= 0) {
+            return false;
+        }
 
         try (Connection connection = ConnectionProvider.connect();
-        PreparedStatement statement = connection.prepareStatement(sql)) {
+                PreparedStatement statement = connection.prepareStatement(sql)) {
             statement.setString(1, deliveryMan.getName());
             statement.setString(2, deliveryMan.getLast_name());
             statement.setInt(3, deliveryMan.getDelivery_man_nr());
@@ -25,25 +28,24 @@ public class DeliveryManController implements IDeliveryManController{
             System.out.println(e.getMessage());
             return false;
         }
-        
+
     }
 
     public List<DeliveryMan> list() {
         List<DeliveryMan> deliveryMans = new ArrayList<>();
 
-        String sql = "SELECT name, last_name, delivery_man_nr " + 
-            "FROM delivery_man";
+        String sql = "SELECT name, last_name, delivery_man_nr " +
+                "FROM delivery_man";
 
         try (Connection connection = ConnectionProvider.connect();
-             Statement statement = connection.createStatement();
-             ResultSet resultSet = statement.executeQuery(sql)){
-            
+                Statement statement = connection.createStatement();
+                ResultSet resultSet = statement.executeQuery(sql)) {
+
             while (resultSet.next()) {
                 deliveryMans.add(new DeliveryMan(
-                    resultSet.getString("name"),
-                    resultSet.getString("last_name"),
-                    resultSet.getInt("delivery_man_nr")
-                ));     
+                        resultSet.getString("name"),
+                        resultSet.getString("last_name"),
+                        resultSet.getInt("delivery_man_nr")));
             }
             connection.close();
         } catch (SQLException e) {
@@ -56,20 +58,20 @@ public class DeliveryManController implements IDeliveryManController{
     public DeliveryMan read(int delivery_man_nr) {
 
         String sql = "SELECT name, last_name, delivery_man_nr " +
-            "FROM delivery_man WHERE delivery_man_nr = ?";
+                "FROM delivery_man WHERE delivery_man_nr = ?";
         DeliveryMan deliveryMan = null;
 
         try (Connection connection = ConnectionProvider.connect();
-             PreparedStatement statement = connection.prepareStatement(sql)){
-            
-            statement.setInt(1,delivery_man_nr);
+                PreparedStatement statement = connection.prepareStatement(sql)) {
 
-            ResultSet resultSet  = statement.executeQuery();
-            
-            if(resultSet.next()) {
-                deliveryMan = new DeliveryMan(resultSet.getString("name"), 
-                resultSet.getString("last_name"), 
-                resultSet.getInt("delivery_man_nr"));
+            statement.setInt(1, delivery_man_nr);
+
+            ResultSet resultSet = statement.executeQuery();
+
+            if (resultSet.next()) {
+                deliveryMan = new DeliveryMan(resultSet.getString("name"),
+                        resultSet.getString("last_name"),
+                        resultSet.getInt("delivery_man_nr"));
             }
             connection.close();
         } catch (SQLException e) {
@@ -81,25 +83,25 @@ public class DeliveryManController implements IDeliveryManController{
     public boolean update(int delivery_man_nr, DeliveryMan deliveryMan) {
 
         String sql = "UPDATE delivery_man " +
-            "SET name = ?, last_name = ? " +
-            "WHERE delivery_man_nr = ?";
+                "SET name = ?, last_name = ? " +
+                "WHERE delivery_man_nr = ?";
 
-        if(validate(delivery_man_nr) == false || deliveryMan == null) {
+        if (validate(delivery_man_nr) == false || deliveryMan == null) {
             return false;
         }
 
         try (Connection connection = ConnectionProvider.connect();
 
-            PreparedStatement statement = connection.prepareStatement(sql)){
-            
-            statement.setString(1,deliveryMan.getName());
-            statement.setString(2,deliveryMan.getLast_name());
-            statement.setInt(3,deliveryMan.getDelivery_man_nr());
+                PreparedStatement statement = connection.prepareStatement(sql)) {
+
+            statement.setString(1, deliveryMan.getName());
+            statement.setString(2, deliveryMan.getLast_name());
+            statement.setInt(3, deliveryMan.getDelivery_man_nr());
 
             statement.executeUpdate();
             connection.close();
             return true;
-            
+
         } catch (SQLException e) {
             System.out.println(e.getMessage());
             return false;
@@ -109,29 +111,29 @@ public class DeliveryManController implements IDeliveryManController{
     public boolean delete(int delivery_man_nr) {
 
         String sql = "DELETE FROM delivery_man " +
-            "WHERE delivery_man_nr = ?";
+                "WHERE delivery_man_nr = ?";
 
-        if(validate(delivery_man_nr) == false) {
+        if (validate(delivery_man_nr) == false) {
             return false;
         }
 
         try (Connection connection = ConnectionProvider.connect();
-            PreparedStatement statement  = connection.prepareStatement(sql)){
-            
-            statement.setInt(1,delivery_man_nr);
+                PreparedStatement statement = connection.prepareStatement(sql)) {
+
+            statement.setInt(1, delivery_man_nr);
 
             statement.executeUpdate();
             connection.close();
             return true;
-            
+
         } catch (SQLException e) {
             System.out.println(e.getMessage());
             return false;
         }
     }
 
-    private boolean validate(int delivery_man_nr){
-        if(read(delivery_man_nr) == null) {
+    private boolean validate(int delivery_man_nr) {
+        if (read(delivery_man_nr) == null) {
             return false;
         }
         return true;
